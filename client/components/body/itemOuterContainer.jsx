14e63ef6cfe7cec9bac6516169855d835a323039
MadinventorZero <dmafduke@gmail.com>
2021-04-03 11:10:30 -0700

    Initial progress in itemOuterContainer in working with React Hooks

--- client/components/body/itemOuterContainer.jsx
+++ client/components/body/itemOuterContainer.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 
 const ItemOuterContainer = (props) => {
+  const [listingsState, setListingsState] = useState({
+    listingsFromDB: [],
 
-  return ();
+  });
+  return (
+    <div id="itemOuterCountainer">
+      {listingsState.listingsFromDB}
+    </div>
+  )
 }
 
 export default ItemOuterContainer;
