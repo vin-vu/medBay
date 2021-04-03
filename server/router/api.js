4746e6f817fea7aea31d13cc24334bb2847edd66
John Lee <jle432edu@gmail.com>
2021-04-03 16:03:08 -0700

    Added router dir with router file

---
+++ server/router/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const models = require('../models/sickBayModels');
+const productController = require('../controllers/productController');
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+
 const app = express();
-const models = require('./models/sickBayModels');
+const api = require('./routes/api');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', api);
+
 // GET ALL
 app.get('/api/allProducts', (req, res) => {
   models.Product.find({})
