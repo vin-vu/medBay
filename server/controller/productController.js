73b2d8861064a6e63dc0e15ae6cd1e90b5939573
John Lee <jle432edu@gmail.com>
2021-04-03 16:13:09 -0700

    Added controller directory and file

---
+++ server/controller/productController.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const models = require('../models/sickBayModels.js');
+
+const productController = {};
+
+productController.itemSearch = (req, res, next) => {
+  
+}
+
+//your code....
+module.exports = productController;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const app = express();
-const api = require('./routes/api');
+const api = require('./routes/api.js');
+const models = require('./models/sickBayModels.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
