00bedcf9ab6c573cb6bee26daf5536b5ed202977
MadinventorZero <dmafduke@gmail.com>
2021-04-03 12:26:43 -0700

    Progress on Body skeleton, item container, and fetches

--- client/components/body/itemOuterContainer.jsx
+++ client/components/body/itemOuterContainer.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as IndividualDisplay from './body/IndividualDisplay';
 
+// '/api/allProducts'
 const ItemOuterContainer = (props) => {
   const [listingsState, setListingsState] = useState({
     listingsFromDB: [],
 
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/allProducts')
+    .then(dbListings => setListingsState({...listingsState, listingsFromDB = dbListings}))
+  });
+
   return (
     <div id="itemOuterCountainer">
       {listingsState.listingsFromDB}
     </div>
-  )
-}
+  );
+};
 
 export default ItemOuterContainer;
--- client/components/mainBody.jsx
+++ client/components/mainBody.jsx
@@ -4,9 +4,15 @@ import * as LoadMoreBtn from './body/LoadMoreBtn';
 import * as IndividualDisplay from './body/IndividualDisplay';
 
 const MainBody = (props) => {
+  const [bodyState, setBodyState] = useState({
 
-  return ()
-}
-
+  });
+  return (
+    <section>
+      <div id="placeholder for ItemOuterContainer component, IndividualDisplay will be a child of this route" ></div>
+      <div id="placeholder for LoadMoreBtn component, IndividualDisplay may also be a child of this route"></div>
+    </section>
+  );
+};
 
 export default MainBody;
