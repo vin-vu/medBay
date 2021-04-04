7bf863cfe43da158749b6be023eae9760078121e
Danny <github@jdanielmartinez.com>
2021-04-04 11:10:47 -0700

    Add HTML for buttons

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,17 +1,23 @@
+// Dependancy imports
 import React, { useState } from 'react';
+import { ButtonGroup, makeStyles } from '@material-ui/core/';
+
+// Local files imports
 import LogoName from './navbar/logoName.jsx';
 // import BigSearch from './navbar/bigSearch';
 // import Categories from './navbar/categories';
-// import Login from './navbar/login';
-// import Signup from './navbar/signup';
+import Login from './navbar/login.jsx';
+import Signup from './navbar/signup.jsx';
 
 const Navbar = (props) => (
   <header id="navbar">
     <div id="top">
       <LogoName />
       <section id="bigSearchPlaceholder">Search bar </section>
-      <section id="loginPlaceholder">Login Button</section>
-      <section id="signupPlaceholder">signUp Button</section>
+      <ButtonGroup variant="contained">
+        <Login />
+        <Signup />
+      </ButtonGroup>
     </div>
     <div id="bottom">
       <section id="categoryPlaceholder">categories</section>
