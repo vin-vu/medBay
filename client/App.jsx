bc21002943a5b29d4ae6b9b77b1733bbd414e1bc
Danny <github@jdanielmartinez.com>
2021-04-05 12:30:19 -0700

    Finish updating Material Theme

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { purple, orange, green } from '@material-ui/core/colors';
 import Navbar from './components/navbar';
 import MainBody from './components/mainBody';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: orange[500],
+    },
+  },
+});
+
 const App = (props) => (
-  <div id="bigBoyBody">
+  <ThemeProvider theme={theme}>
     <Navbar />
     <MainBody />
-  </div>
+  </ThemeProvider>
 );
 
 export default App;
