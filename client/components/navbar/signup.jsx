fe2c5aa2ab9db10367bd3375a42b2bbb9c0921b8
MadinventorZero <dmafduke@gmail.com>
2021-04-05 13:56:12 -0700

    Initial framework for Popover

--- client/components/navbar/signup.jsx
+++ client/components/navbar/signup.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import Popover from '@material-ui/core/Popover';
+import {
+  usePopupState,
+  bindTrigger,
+  bindPopover,
+} from 'material-ui-popup-state/hooks';
 
 const Signup = (props) => (
   <Button onClick={(e) => alert('Login Button Clicked!')}>
