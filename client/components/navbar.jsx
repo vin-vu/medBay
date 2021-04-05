8aa1ca0052876fc296927f65146780988d84ad2a
Danny <github@jdanielmartinez.com>
2021-04-05 13:08:33 -0700

    Add Easter Egg

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = (props) => {
       <AppBar>
         <Toolbar>
           <Box className={classes.logo}>
-            <img src="../assets/sickBay.jpg" alt="Logo" width="70px" height="70px" style={{ 'border-radius': '15px' }} />
+            <a href="https://bit.ly/31UNEiV" target="_blank" rel="noreferrer"><img src="../assets/sickBay.jpg" alt="Logo" width="70px" height="70px" style={{ 'border-radius': '15px' }} /></a>
           </Box>
           <LogoName />
           <div className={classes.grow} />
