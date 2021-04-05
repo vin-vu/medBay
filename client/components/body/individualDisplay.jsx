417007e4eb21cc28c7619dcca19e5a5d2216c676
MadinventorZero <dmafduke@gmail.com>
2021-04-04 23:58:22 -0700

    Completed a working fetch reqeust to allProducts, Successfully created all IndividualDisplay elements, succesfully updated state, successfully rendered all object retrieved from the database, confirmed callback functionality is passed through to button

--- client/components/body/individualDisplay.jsx
+++ client/components/body/individualDisplay.jsx
@@ -1,7 +1,44 @@
 import React, { useState } from 'react';
+import { styled } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+const AddToCartButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'White',
+  height: 36,
+  padding: '0 30px',
+});
 
 const IndividualDisplay = (props) => (
-  <div id="placeholder for individual listings rendering" />
+  <div id={props.key}>
+    <img src={props.image} alt='Image not Found'/>
+    <span>
+      Product:
+      {' '}
+      {props.title}
+    </span>
+    <span>
+      Category:
+      {' '}
+      {props.category}
+    </span>
+    <span>
+      Description:
+      {' '}
+      {props.description}
+    </span>
+    <span>
+      Price:
+      {' '}
+      {props.price}
+    </span>
+    <AddToCartButton onClick={props.buttonFunc}>Add To Cart</AddToCartButton>
+  </div>
+
 );
 
 export default IndividualDisplay;
--- client/components/body/itemOuterContainer.jsx
+++ client/components/body/itemOuterContainer.jsx
@@ -4,22 +4,47 @@ import IndividualDisplay from './individualDisplay.jsx';
 // '/api/allProducts'
 const ItemOuterContainer = (props) => {
   const [listingsState, setListingsState] = useState({
-    listingsFromDB: [],
-
+    listingsFromDb: [],
+    listingsToRender: [],
+    currentListings: 0,
+    addToCart: () => console.log('I\'ve been added to cart!!!'),
   });
 
+  // React function to assemble a bunch of listing elements and can work in any component
+  const createListingElements = (listingArray, cartCallback) => {
+    const renderListings = [];
+    let currentNumber = 0;
+    for (const element of listingArray) {
+      renderListings.push(<IndividualDisplay
+        title={element.Title}
+        description={element.Description}
+        category={element.Category}
+        image={element.ImageURL}
+        price={element.Price}
+        buttonFunc={cartCallback}
+        key={`${currentNumber + 1}`}
+      />);
+      currentNumber += 1;
+    }
+    const results = { listings: renderListings, listingsCreated: currentNumber };
+    return results;
+  };
+
+  // Will be expecting an image, title, description, price, category, and quantity
   useEffect(() => {
-    fetch('http://localhost:3000/api/allProducts')
+    fetch('/api/allProducts')
+      .then((response) => response.json())
       .then((dbListings) => {
-        setListingsState({ ...listingsState, listingsFromDB: dbListings });
-      });
-  });
+        console.log(dbListings);
+        const listingData = createListingElements(dbListings, listingsState.addToCart);
+        setListingsState({ ...listingsState, listingsToRender: listingData.listings, currentListings: listingData.listingsCreated, listingsFromDb: dbListings  });
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  // In the end I'll be feeding in an array of preconstructed IndividualDisplay
-  // components feeding in the props from state
   return (
     <div id="itemOuterCountainer">
-      <IndividualDisplay />
+      {listingsState.listingsToRender}
     </div>
   );
 };
