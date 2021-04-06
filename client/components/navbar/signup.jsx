78e5f3907e606c94806f4e883b606b377d415098
MadinventorZero <dmafduke@gmail.com>
2021-04-05 17:29:35 -0700

    Signup post verified, but not processing response, Login configured but not verified

--- client/components/navbar/signup.jsx
+++ client/components/navbar/signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Popover from '@material-ui/core/Popover';
 import {
@@ -7,11 +9,50 @@ import {
   bindTrigger,
   bindPopover,
 } from 'material-ui-popup-state/hooks';
+import { TextField } from '@material-ui/core';
 
-const Signup = (props) => (
-  <Button onClick={(e) => alert('Login Button Clicked!')} color="secondary" variant="contained">
-    Sign Up
-  </Button>
-);
+const styles = (theme) => ({
+  typography: {
+    margin: theme.spacing(2),
+  },
+});
 
-export default Signup;
+const SignupPopupState = ({ classes }) => {
+  const popupState = usePopupState({
+    variant: 'popover',
+    popupId: 'signup',
+  });
+  return (
+    <div>
+      <Button onClick={(e) => console.log('Signup Button Clicked!')} color="secondary" variant="contained" {...bindTrigger(popupState)}>
+        Sign Up
+      </Button>
+      <Popover
+        {...bindPopover(popupState)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>
+          <form action="/api/signup" method="POST">
+            <TextField required id="standard-required" label="Username Required" type="text" defaultValue="Username" name="username" />
+            <TextField required id="standard-password-input" label="Password Required" type="password" defaultValue="" name="password" />
+            <TextField required id="standard-password-input" label="Repeat Required" type="password" defaultValue="" name="psw-repeat" />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Typography>
+      </Popover>
+    </div>
+  );
+};
+
+SignupPopupState.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SignupPopupState);
